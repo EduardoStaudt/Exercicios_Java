6e668217894e5b1f82e791671b71543512593ea4
Eduardo Staudt <programadorstaudt@gmail.com>
2025-08-03 17:37:56 -0300

    Primeiro commit para os Exercicios

---
+++ src/main/java/org/example/ListaUm.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ListaUm {
+    public static void main(String[] args) {
+        var nome = "John";
+        System.out.println("Hello, " + nome + "!");
+    }
+}
